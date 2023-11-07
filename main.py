d865654d637ebcebfe2797131cc5c13b3ad01b36
Stanlito-AI <mosesstanley99@gmail.com>
2023-11-07 10:12:17 -1000

    Mlflow

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from Mlops import logger
 from Mlops.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from Mlops.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from Mlops.pipeline.stage_03_training import TrainingPipeline
+from Mlops.pipeline.stage_04_model_evaluation import EvaluationPipeline
 
 # from Mlops.pipeline.stage_03_model_training import ModelTrainingPipeline
 # sudo apt install nvidia-cuda-toolkit
@@ -38,6 +39,20 @@ try:
     logger.info(f"*******************")
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = TrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed "
+                f"<<<<<<\n\nx==========+++++++++++++++++++++++++++++=========================x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Model Evaluation"
+
+try:
+    logger.info(f"*******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = EvaluationPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed "
                 f"<<<<<<\n\nx==========+++++++++++++++++++++++++++++=========================x")
--- src/Mlops/config/configuration.py
+++ src/Mlops/config/configuration.py
@@ -63,3 +63,14 @@ class ConfigurationManager:
 
         return training_config
 
+    def get_evaluation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model="artifacts/training/model.h5",
+            training_data="artifacts/data_ingestion/kidney_dataset",
+            mlflow_uri="https://dagshub.com/Stanlito-AI/Mlops_kidney_disease.mlflow ",
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
+
