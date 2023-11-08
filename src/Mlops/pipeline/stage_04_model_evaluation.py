ce893c6a290463797d026038d92f7ba300720f25
Stanlito-AI <mosesstanley99@gmail.com>
2023-11-07 20:45:06 -1000

    Mlflow

---
+++ src/Mlops/pipeline/stage_04_model_evaluation.py
@@ -0,0 +1,32 @@
+from Mlops.components.model_evaluation import Evaluation
+from Mlops.config.configuration import ConfigurationManager
+
+from Mlops import logger
+
+STAGE_NAME = "Evaluation stage"
+
+
+class EvaluationPipeline:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def main():
+        config = ConfigurationManager()
+        eval_config = config.get_evaluation_config()
+        evaluation = Evaluation(eval_config)
+        evaluation.evaluation()
+        evaluation.save_score()
+        evaluation.log_into_mlflow()
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f"*******************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = EvaluationPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
